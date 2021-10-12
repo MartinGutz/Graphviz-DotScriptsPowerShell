a2ccbba55d086a21d06df6a68ee94bc7747994ae
MartinGutz <MartinGutz@gmail.com>
2021-10-11 17:12:18 -0700

    Update CheckGraphFileFormat.Tests.ps1
    Updated the tests to check the whole file for regular expressions to check for graph lines and color lines

--- DotNet Creator/tests/pesterTests/CheckGraphFileFormat.Tests.ps1
+++ DotNet Creator/tests/pesterTests/CheckGraphFileFormat.Tests.ps1
@@ -1,11 +1,6 @@
-Describe "Check the File Exists" {
-    #    Context "Checking the Header of the file" {
-    #        $file = "C:\Users\Administrator\Desktop\Tools\DotExamples\Ouptut\outputfile.gv"
-    #        Write-Host "Testing"
-    #        It (Get-Content $file)[0] | 
-    #        Should -Match 'test' #'digraph \w.*\{'
-    #    }
-        It 'File Should Exist' {
+Describe "File and Format Checks" {
+
+    It 'File Should Exist' {
             "..\output\result.gv" |
             Should -Exist
         }
@@ -14,6 +9,15 @@ Describe "Check the File Exists" {
             $file = "C:\Users\Administrator\Desktop\Tools\DotExamples\Ouptut\outputfile.gv"
             (Get-Content $file)[0] | Should -Match 'digraph \w.*\{'
             (Get-Content $file)[$file.Count - 1] | Should -Match '\{'
+        }
+
+        It 'Check the file contents' {
+            $file = Get-Content -Path "C:\Users\Administrator\Desktop\Tools\DotExamples\Ouptut\outputfile.gv"
+            $fileLength = $file.Count - 1
+            for($counter = 1;$counter -lt $fileLength; $counter++ )
+            {
+                $file[$counter] | Should -Match '\w*? [-][>] \w*|\w* [[]\w*[=]["]\w*["][]]'
+            }
         }
     
     }
