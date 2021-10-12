ebc7836c1368896f8c25e4587b27e04d3b3f98c3
MartinGutz <MartinGutz@gmail.com>
2021-10-11 17:26:12 -0700

    Add Color Set Validation
    Added a set of the available colors for SVG. The colors are located here https://graphviz.org/doc/info/colors.html#svg

--- DotNet Creator/src/CreateDotNetFile.ps1
+++ DotNet Creator/src/CreateDotNetFile.ps1
@@ -25,8 +25,15 @@ function ConnectNodes($parentNode, $childNode)
     $global:graphFileContents += "`n`t$parentNode -> $childNode"
 }
 
-function SetNodeColor($node, $color)
+function SetNodeColor()
 {
+    [CmdletBinding()]
+    Param(
+    [string]$node,
+    [ValidateSet("aliceblue","antiquewhite","aqua","aquamarine","azure","beige","bisque","black","blanchedalmond","blue","blueviolet","brown","burlywood","cadetblue","chartreuse","chocolate","coral","cornflowerblue","cornsilk","crimson","cyan","darkblue","darkcyan","darkgoldenrod","darkgray","darkgreen","darkgrey","darkkhaki","darkmagenta","darkolivegreen","darkorange","darkorchid","darkred","darksalmon","darkseagreen","darkslateblue","darkslategray","darkslategrey","darkturquoise","darkviolet","deeppink","deepskyblue","dimgray","dimgrey","dodgerblue","firebrick","floralwhite","forestgreen","fuchsia","gainsboro","ghostwhite","gold","goldenrod","gray","grey","green","greenyellow","honeydew","hotpink","indianred","indigo","ivory","khaki","lavender","lavenderblush","lawngreen","lemonchiffon","lightblue","lightcoral","lightcyan","lightgoldenrodyellow","lightgray","lightgreen","lightgrey","lightpink","lightsalmon","lightseagreen","lightskyblue","lightslategray","lightslategrey","lightsteelblue","lightyellow","lime","limegreen","linen","magenta","maroon","mediumaquamarine","mediumblue","mediumorchid","mediumpurple","mediumseagreen","mediumslateblue","mediumspringgreen","mediumturquoise","mediumvioletred","midnightblue","mintcream","mistyrose","moccasin","navajowhite","navy","oldlace","olive","olivedrab","orange","orangered","orchid","palegoldenrod","palegreen","paleturquoise","palevioletred","papayawhip","peachpuff","peru","pink","plum","powderblue","purple","red","rosybrown","royalblue","saddlebrown","salmon","sandybrown","seagreen","seashell","sienna","silver","skyblue","slateblue","slategray","slategrey","snow","springgreen","steelblue","tan","teal","thistle","tomato","turquoise","violet","wheat","white","whitesmoke","yellow","yellowgreen")]
+    [string]$color
+    )
+
     $global:graphFileContents += "`n`t$node [color=`"$color`"]"
 }
 
@@ -35,10 +42,10 @@ $global:graphFileContents = CreateDotGraphHeader $GraphName;
 ConnectNodes "firstnode" "childnode1"
 ConnectNodes "firstnode" "childnode2"
 ConnectNodes "firstnode" "childnode3"
-SetNodeColor "firstnode" "green"
+SetNodeColor -Node "firstnode" -Color "green"
 ConnectNodes "childnode2" "childnode21"
 ConnectNodes "childnode2" "childnode22"
 ConnectNodes "childnode3" "childnode31"
-SetNodeColor "childnode2" "red"
+SetNodeColor -Node "childnode2" -Color "red"
 $global:graphFileContents = CreateDotGraphFooter $global:graphFileContents
 Set-Content -Value $global:graphFileContents $OutputFile
--- DotNet Creator/tests/RunPesterScripts.ps1
+++ DotNet Creator/tests/RunPesterScripts.ps1
@@ -1,10 +1,11 @@
 Import-Module "C:\Users\Administrator\Desktop\Tools\PowerShell Gallery\Pester.psm1"
 #CreateDotNetFile.ps1
-..\src\CreateDotNetFile.ps1 -GraphName "SampleGraph" -OutputFile "..\output\result.gv"
+
 
 while($true)
 {
     Clear-Host
+    ..\src\CreateDotNetFile.ps1 -GraphName "SampleGraph" -OutputFile "..\output\result.gv"
     Invoke-Pester -Path ".\pesterTests"
     Start-Sleep -Seconds 5    
 }
